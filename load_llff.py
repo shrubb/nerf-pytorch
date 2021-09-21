Implement VolSDF's camera recentering and normalization, limit SDF to sphere; fix eikonal loss; add RAdam

load_llff.py
@@ -2,7 +2,7 @@ import numpy as np
 import os, imageio
 
 
-########## Slightly modified version of LLFF data loading code 
+########## Slightly modified version of LLFF data loading code
 ##########  see https://github.com/Fyusion/LLFF for original
 
 def _minify(basedir, factors=[], resolutions=[]):
@@ -17,15 +17,15 @@ def _minify(basedir, factors=[], resolutions=[]):
             needtoload = True
     if not needtoload:
         return
-    
+
     from shutil import copy
     from subprocess import check_output
-    
+
     imgdir = os.path.join(basedir, 'images')
     imgs = [os.path.join(imgdir, f) for f in sorted(os.listdir(imgdir))]
     imgs = [f for f in imgs if any([f.endswith(ex) for ex in ['JPG', 'jpg', 'png', 'jpeg', 'PNG']])]
     imgdir_orig = imgdir
-    
+
     wd = os.getcwd()
 
     for r in factors + resolutions:
@@ -38,89 +38,89 @@ def _minify(basedir, factors=[], resolutions=[]):
         imgdir = os.path.join(basedir, name)
         if os.path.exists(imgdir):
             continue
-            
+
         print('Minifying', r, basedir)
-        
+
         os.makedirs(imgdir)
         check_output('cp {}/* {}'.format(imgdir_orig, imgdir), shell=True)
-        
+
         ext = imgs[0].split('.')[-1]
         args = ' '.join(['mogrify', '-resize', resizearg, '-format', 'png', '*.{}'.format(ext)])
         print(args)
         os.chdir(imgdir)
         check_output(args, shell=True)
         os.chdir(wd)
-        
+
         if ext != 'png':
             check_output('rm {}/*.{}'.format(imgdir, ext), shell=True)
             print('Removed duplicates')
         print('Done')
-            
-        
-        
-        
+
+
+
+
 def _load_data(basedir, factor=None, width=None, height=None, load_imgs=True):
-    
+
     poses_arr = np.load(os.path.join(basedir, 'poses_bounds.npy'))
     poses = poses_arr[:, :-2].reshape([-1, 3, 5]).transpose([1,2,0])
     bds = poses_arr[:, -2:].transpose([1,0])
-    
+
     img0 = [os.path.join(basedir, 'images', f) for f in sorted(os.listdir(os.path.join(basedir, 'images'))) \
             if f.endswith('JPG') or f.endswith('jpg') or f.endswith('png')][0]
     sh = imageio.imread(img0).shape
-    
-    sfx = ''
-    
+
+    suffix = ''
+
     if factor is not None:
-        sfx = '_{}'.format(factor)
+        suffix = '_{}'.format(factor)
         _minify(basedir, factors=[factor])
         factor = factor
     elif height is not None:
         factor = sh[0] / float(height)
         width = int(sh[1] / factor)
         _minify(basedir, resolutions=[[height, width]])
-        sfx = '_{}x{}'.format(width, height)
+        suffix = '_{}x{}'.format(width, height)
     elif width is not None:
         factor = sh[1] / float(width)
         height = int(sh[0] / factor)
         _minify(basedir, resolutions=[[height, width]])
-        sfx = '_{}x{}'.format(width, height)
+        suffix = '_{}x{}'.format(width, height)
     else:
         factor = 1
-    
-    imgdir = os.path.join(basedir, 'images' + sfx)
+
+    imgdir = os.path.join(basedir, 'images' + suffix)
     if not os.path.exists(imgdir):
         print( imgdir, 'does not exist, returning' )
         return
-    
+
     imgfiles = [os.path.join(imgdir, f) for f in sorted(os.listdir(imgdir)) if f.endswith('JPG') or f.endswith('jpg') or f.endswith('png')]
     if poses.shape[-1] != len(imgfiles):
         print( 'Mismatch between imgs {} and poses {} !!!!'.format(len(imgfiles), poses.shape[-1]) )
         return
-    
+
     sh = imageio.imread(imgfiles[0]).shape
     poses[:2, 4, :] = np.array(sh[:2]).reshape([2, 1])
     poses[2, 4, :] = poses[2, 4, :] * 1./factor
-    
+
     if not load_imgs:
         return poses, bds
-    
+
     def imread(f):
         if f.endswith('png'):
             return imageio.imread(f, ignoregamma=True)
         else:
             return imageio.imread(f)
-        
-    imgs = imgs = [imread(f)[...,:3]/255. for f in imgfiles]
-    imgs = np.stack(imgs, -1)  
-    
+
+    imgs = [imread(f)[...,:3]/255. for f in imgfiles]
+    imgs = np.stack(imgs, -1)
+
     print('Loaded image data', imgs.shape, poses[:,-1,0])
     return poses, bds, imgs
 
-    
-            
-            
-    
+
+
+
+
 
 def normalize(x):
     return x / np.linalg.norm(x)
@@ -137,15 +137,60 @@ def ptstocam(pts, c2w):
     tt = np.matmul(c2w[:3,:3].T, (pts-c2w[:3,3])[...,np.newaxis])[...,0]
     return tt
 
-def poses_avg(poses):
+def poses_center(poses, kind='mean'):
+    """
+    Find the 'center' of a set of camera poses.
 
+    poses:
+        np.ndarray, shape: at least N x 3 x 4
+    intersect_principal_axes:
+        str
+        If 'none', return origin ([0, 0, 0]).
+        If 'mean', compute just the average of camera positions.
+        If 'intersection', compute the least squares solution for the
+        intersection point of cameras' principal axes.
+    """
     hwf = poses[0, :3, -1:]
 
-    center = poses[:, :3, 3].mean(0)
+    if kind == 'none':
+        c2w = np.eye(3, 5, dtype=np.float32)
+        c2w[:, 4] = hwf
+        return c2w
+
+    if kind == 'intersection':
+        camera_origins = poses[:, :3, 3]
+        principal_axes = -poses[:, :3, 2]
+        # Solve the linear system:
+        # for all i:
+        #     origins[i].{x,y,z} + t[i] * princ_axes[i].{x,y,z} = center.{x,y,z}
+        #     -center.{x,y,z} + t[i] * princ_axes[i].{x,y,z} = -origins[i].{x,y,z}
+        # a @ solution =~= b
+        # where:
+        # solution = [center.x, center.y, center.z, t[0], t[1], ..., t[N-1]]
+        # a = [
+        #   [-1, 0, 0, princ_axes[0].x, 0, 0, ..., 0],
+        #   [0, -1, 0, princ_axes[0].y, 0, 0, ..., 0],
+        #   [0, 0, -1, princ_axes[0].z, 0, 0, ..., 0],
+        #   [-1, 0, 0, 0, princ_axes[1].x, 0, ..., 0],
+        #   ...
+        # ]
+        # b = [-origins[0].x, -origins[0].y, -origins[0].z, -origins[1].x, ...]
+        a = np.zeros((3 * len(poses), 3 + len(poses)), dtype=np.float32)
+        for dim in range(3): # x, y, or z
+            a[dim::3, dim] = -1
+            np.fill_diagonal(a[dim::3, 3:], principal_axes[:, dim])
+        b = -camera_origins.flatten()
+        solution, _, _, _ = np.linalg.lstsq(a, b)
+        center = solution[:3]
+    elif kind == 'mean':
+        center = poses[:, :3, 3].mean(0)
+    else:
+        raise ValueError(kind)
+
     vec2 = normalize(poses[:, :3, 2].sum(0))
     up = poses[:, :3, 1].sum(0)
     c2w = np.concatenate([viewmatrix(vec2, up, center), hwf], 1)
-    
+
     return c2w
 
 
@@ -154,20 +199,22 @@ def render_path_spiral(c2w, up, rads, focal, zdelta, zrate, rots, N):
     render_poses = []
     rads = np.array(list(rads) + [1.])
     hwf = c2w[:,4:5]
-    
+
     for theta in np.linspace(0., 2. * np.pi * rots, N+1)[:-1]:
-        c = np.dot(c2w[:3,:4], np.array([np.cos(theta), -np.sin(theta), -np.sin(theta*zrate), 1.]) * rads) 
+        c = np.dot(c2w[:3,:4], np.array([np.cos(theta), -np.sin(theta), -np.sin(theta*zrate), 1.]) * rads)
         z = normalize(c - np.dot(c2w[:3,:4], np.array([0,0,-focal, 1.])))
         render_poses.append(np.concatenate([viewmatrix(z, up, c), hwf], 1))
     return render_poses
-    
 
 
-def recenter_poses(poses):
+
+def recenter_poses(poses, kind='mean'):
+    if kind == 'none':
+        return poses
 
     poses_ = poses+0
     bottom = np.reshape([0,0,0,1.], [1,4])
-    c2w = poses_avg(poses)
+    c2w = poses_center(poses, kind=kind)
     c2w = np.concatenate([c2w[:3,:4], bottom], -2)
     bottom = np.tile(np.reshape(bottom, [1,1,4]), [poses.shape[0],1,1])
     poses = np.concatenate([poses[:,:3,:4], bottom], -2)
@@ -182,9 +229,9 @@ def recenter_poses(poses):
 
 
 def spherify_poses(poses, bds):
-    
+
     p34_to_44 = lambda p : np.concatenate([p, np.tile(np.reshape(np.eye(4)[-1,:], [1,1,4]), [p.shape[0], 1,1])], 1)
-    
+
     rays_d = poses[:,:3,2:3]
     rays_o = poses[:,:3,3:4]
 
@@ -195,7 +242,7 @@ def spherify_poses(poses, bds):
         return pt_mindist
 
     pt_mindist = min_line_dist(rays_o, rays_d)
-    
+
     center = pt_mindist
     up = (poses[:,:3,3] - center).mean(0)
 
@@ -208,17 +255,17 @@ def spherify_poses(poses, bds):
     poses_reset = np.linalg.inv(p34_to_44(c2w[None])) @ p34_to_44(poses[:,:3,:4])
 
     rad = np.sqrt(np.mean(np.sum(np.square(poses_reset[:,:3,3]), -1)))
-    
+
     sc = 1./rad
     poses_reset[:,:3,3] *= sc
     bds *= sc
     rad *= sc
-    
+
     centroid = np.mean(poses_reset[:,:3,3], 0)
     zh = centroid[2]
     radcircle = np.sqrt(rad**2-zh**2)
     new_poses = []
-    
+
     for th in np.linspace(0.,2.*np.pi, 120):
 
         camorigin = np.array([radcircle * np.cos(th), radcircle * np.sin(th), zh])
@@ -233,40 +280,69 @@ def spherify_poses(poses, bds):
         new_poses.append(p)
 
     new_poses = np.stack(new_poses, 0)
-    
+
     new_poses = np.concatenate([new_poses, np.broadcast_to(poses[0,:3,-1:], new_poses[:,:3,-1:].shape)], -1)
     poses_reset = np.concatenate([poses_reset[:,:3,:4], np.broadcast_to(poses[0,:3,-1:], poses_reset[:,:3,-1:].shape)], -1)
-    
+
     return poses_reset, new_poses, bds
-    
 
-def load_llff_data(basedir, factor=8, recenter=True, bd_factor=.75, spherify=False, path_zflat=False):
-    
 
+def load_llff_data(
+    basedir, factor=8, recenter='mean', bd_factor=.75, spherify=False, path_zflat=False, r=None):
+    """
+    basedir:
+        `str`
+    factor:
+        `int` (?)
+        Input images will be downsapled by this factor and resaved.
+    recenter:
+        `str`
+        'none', 'mean', or 'intersection'. See help for '--recenter' in 'run_nerf.py'.
+    bd_factor:
+        `float`
+        Rescale the scene by this value. See help for '--rescale_factor' in 'run_nerf.py'.
+    spherify:
+        `bool`
+        Don't know what this does. See help for '--spherify' in 'run_nerf.py'.
+    path_zflat:
+        `bool`
+        Don't know what this does.
+    r:
+        `float`
+        Scene boundary parameter. See help for '--r' in 'run_nerf.py'.
+    """
     poses, bds, imgs = _load_data(basedir, factor=factor) # factor=8 downsamples original imgs by 8x
     print('Loaded', basedir, bds.min(), bds.max())
-    
+
     # Correct rotation matrix ordering and move variable dim to axis 0
     poses = np.concatenate([poses[:, 1:2, :], -poses[:, 0:1, :], poses[:, 2:, :]], 1)
     poses = np.moveaxis(poses, -1, 0).astype(np.float32)
     imgs = np.moveaxis(imgs, -1, 0).astype(np.float32)
     images = imgs
     bds = np.moveaxis(bds, -1, 0).astype(np.float32)
-    
-    # Rescale if bd_factor is provided
-    sc = 1. if bd_factor is None else 1./(bds.min() * bd_factor)
-    poses[:,:3,3] *= sc
-    bds *= sc
-    
-    if recenter:
-        poses = recenter_poses(poses)
-        
+
+    poses = recenter_poses(poses, kind=recenter)
+
+    # Rescale
+    if bd_factor is None:
+        scale = 1.0
+    elif bd_factor < 0:
+        # Apply the camera normalization procedure described in VolSDF section B.1
+        assert r is not None
+        max_camera_distance = np.linalg.norm(poses[:,:3,3], 2, axis=-1).max()
+        scale = r / (max_camera_distance * 1.1)
+    else:
+        scale = 1.0 / (bds.min() * bd_factor)
+
+    poses[:,:3,3] *= scale
+    bds *= scale
+
     if spherify:
         poses, render_poses, bds = spherify_poses(poses, bds)
 
     else:
-        
-        c2w = poses_avg(poses)
+
+        c2w = poses_center(poses, kind=recenter)
         print('recentered', c2w.shape)
         print(c2w[:3,:4])
 
@@ -298,18 +374,18 @@ def load_llff_data(basedir, factor=8, recenter=True, bd_factor=.75, spherify=Fal
 
         # Generate poses for spiral path
         render_poses = render_path_spiral(c2w_path, up, rads, focal, zdelta, zrate=.5, rots=N_rots, N=N_views)
-        
-        
+
+
     render_poses = np.array(render_poses).astype(np.float32)
 
-    c2w = poses_avg(poses)
+    c2w = poses_center(poses, kind=recenter)
     print('Data:')
     print(poses.shape, images.shape, bds.shape)
-    
+
     dists = np.sum(np.square(c2w[:3,3] - poses[:,:3,3]), -1)
     i_test = np.argmin(dists)
     print('HOLDOUT view is', i_test)
-    
+
     images = images.astype(np.float32)
     poses = poses.astype(np.float32)
 

nerf.py
@@ -33,6 +33,9 @@ def batchify(fn, chunk_size):
         outputs_flat = [torch.cat(x, 0) for x in zip(*outputs_flat)]
         # Reshape them like original inputs
         outputs = [x.reshape(*original_batch_shape, x.shape[-1]) for x in outputs_flat]
+        # If there was one tensor, don't return it in a list
+        if len(outputs) == 1:
+            outputs = outputs[0]
 
         return outputs
 
@@ -147,7 +150,7 @@ def create_model(args):
     render_kwargs_test['perturb'] = False
     render_kwargs_test['raw_noise_std'] = 0.
 
-    return render_kwargs_train, render_kwargs_test, start, grad_vars, optimizer
+    return render_kwargs_train, render_kwargs_test, start, optimizer
 
 
 def raw2outputs(
@@ -213,7 +216,6 @@ def render_rays(ray_batch, # of length <= `args.chunk`
                 network_fine=None,
                 white_bkgd=False,
                 raw_noise_std=0.,
-                verbose=False,
                 pytest=False):
     """Volumetric rendering.
     Args:
@@ -233,7 +235,6 @@ def render_rays(ray_batch, # of length <= `args.chunk`
       network_fine: "fine" network with same spec as network_fn.
       white_bkgd: bool. If True, assume a white background.
       raw_noise_std: ...
-      verbose: bool. If True, print more debugging info.
     Returns:
       rgb_map: [num_rays, 3]. Estimated RGB color of a ray. Comes from fine model.
       disp_map: [num_rays]. Disparity map. 1 / depth.

run_nerf.py
@@ -219,9 +219,10 @@ def render_path(
 def load_dataset(args):
     K = None
     if args.dataset_type == 'llff':
-        images, poses, bds, render_poses, i_test = load_llff_data(args.datadir, args.factor,
-                                                                  recenter=True, bd_factor=.75,
-                                                                  spherify=args.spherify)
+        images, poses, bds, render_poses, i_test = load_llff_data(
+            args.datadir, args.factor, recenter=args.recenter, bd_factor=args.rescale_factor,
+            spherify=args.spherify, r=args.r if args.rescale_factor < 0 else None)
+
         hwf = poses[0,:3,-1]
         poses = poses[:,:3,:4]
         print('Loaded llff', images.shape, render_poses.shape, hwf, args.datadir)
@@ -329,6 +330,8 @@ def config_parser():
                         help='batch size (number of random rays per gradient step)')
     parser.add_argument("--num_iterations", type=int, default=200000 + 1,
                         help='total number of optimization steps')
+    parser.add_argument("--optimizer", type=str, choices=['adam', 'radam'], default='adam',
+                        help='Optimizer')
     parser.add_argument("--lrate", type=float, default=5e-4,
                         help='learning rate')
     parser.add_argument("--lrate_decay", type=int, default=250,
@@ -339,6 +342,8 @@ def config_parser():
                         help='number of pts sent through network in parallel, decrease if running out of memory')
     parser.add_argument("--no_batching", action='store_true',
                         help='only take random rays from 1 image at a time')
+    parser.add_argument("--eikonal_loss_weight", type=float, default=0.1,
+                        help="VolSDF only: lambda from equation 17")
     parser.add_argument("--no_reload", action='store_true',
                         help='do not reload weights from saved ckpt')
     parser.add_argument("--ft_path", type=str, default=None,
@@ -354,6 +359,8 @@ def config_parser():
                         help='number of additional fine samples per ray')
     parser.add_argument("--perturb", type=float, default=1.,
                         help='set to 0. for no jitter, 1. for jitter')
+    parser.add_argument("--r", type=float, default=3.0,
+                        help="VolSDF only: 'r', scene boundary parameter from equation (19).")
     parser.add_argument("--use_viewdirs", action='store_true',
                         help='use full 5D input instead of 3D')
     parser.add_argument("--fixed_viewdir_in_test",
@@ -415,6 +422,22 @@ def config_parser():
                         help='set for spherical 360 scenes')
     parser.add_argument("--llffhold", type=int, default=8,
                         help='will take every 1/N images as LLFF test set, paper uses 8')
+    parser.add_argument("--recenter", type=str, default='mean',
+                        choices=['none', 'mean', 'intersection'],
+                        help="Only for --dataset_type=llff.\n"
+                             "If 'none', leave camera poses as is.\n"
+                             "If 'mean', move the scene origin to the average "
+                             "camera position.\n"
+                             "If 'intersection', compute the least squares solution for the "
+                             "intersection point of cameras' principal axes and move the scene "
+                             "origin there.\n"
+                             "See also: '--r'.")
+    parser.add_argument("--rescale_factor", type=float, default=0.75,
+                        help="Only for --dataset_type=llff.\n"
+                             "Rescale the entire scene (i.e. cameras' coordinates) by approx. "
+                             "this value.\n"
+                             "If negative value is given, follow the camera normalization "
+                             "procedure described in VolSDF section B.1, using '--r'.")
 
     # logging/saving options
     parser.add_argument("--i_print",   type=int, default=100,
@@ -472,7 +495,7 @@ def train():
         'nerf': nerf,
         'volsdf': volsdf
     }[args.model]
-    render_kwargs_train, render_kwargs_test, start, grad_vars, optimizer = \
+    render_kwargs_train, render_kwargs_test, start, optimizer = \
         model_specific_module.create_model(args)
 
     bds_dict = {
@@ -509,6 +532,7 @@ def train():
             # Save camera parameters too
             save_blender_poses(testsavedir / "cameras.json", render_poses.cpu().numpy(), hwf)
 
+            render_kwargs_test['network_fn'].eval()
             rgbs, _ = render_path(
                 render_poses, hwf, K, args.chunk, render_kwargs_test, gt_imgs=images,
                 savedir=testsavedir, render_factor=args.render_factor,
@@ -551,6 +575,8 @@ def train():
 
     for global_step in range(start, args.num_iterations + 1):
         # First, validate
+        render_kwargs_test['network_fn'].eval()
+
         if global_step % args.i_weights == 0 and global_step > 0 or global_step == args.num_iterations:
             dict_to_save = {
                 'global_step': global_step,
@@ -593,7 +619,7 @@ def train():
                     render_kwargs_test, gt_imgs=images[i_test], savedir=testsavedir)
             print('Saved test set')
 
-        if global_step % args.i_img == 0 or global_step == args.num_iterations:
+        if global_step % args.i_img == 0 and global_step > 0 or global_step == args.num_iterations:
             # Log a rendered validation view to Tensorboard
             val_image_indices = [i_val[0], i_val[-1]]
             targets = images[val_image_indices] # N x H x W x C, np.float64, 0..1
@@ -623,6 +649,7 @@ def train():
             #         tf.contrib.summary.image('disp0', extras['disp0'][tf.newaxis,...,tf.newaxis])
             #         tf.contrib.summary.image('z_std', extras['z_std'][tf.newaxis,...,tf.newaxis])
 
+        render_kwargs_test['network_fn'].train()
 
         time0 = time.time()
 
@@ -673,18 +700,18 @@ def train():
 
         #####  Core optimization loop  #####
         rgb, disp, acc, extras = render(H, W, K, chunk=args.chunk, rays=batch_rays,
-                                                verbose=global_step < 10, retraw=True,
-                                                **render_kwargs_train)
+                                                retraw=True, **render_kwargs_train)
 
         optimizer.zero_grad()
         img_loss = img2mse(rgb, target_s)
         loss = img_loss
         psnr = mse2psnr(img_loss)
 
+        # Add eikonal loss
         if args.model == 'volsdf':
-            eikonal_loss = ((extras['sdf_normal'] - 1.0) ** 2).mean()
-            eikonal_loss_weight = 0.1 # aka lambda from the paper
-            loss += eikonal_loss_weight * eikonal_loss
+            assert extras['sdf_normal'].shape[-1] == 3
+            eikonal_loss = ((extras['sdf_normal'].norm(2, dim=-1) - 1.0) ** 2).mean()
+            loss += args.eikonal_loss_weight * eikonal_loss
 
         if 'rgb0' in extras:
             img_loss0 = img2mse(extras['rgb0'], target_s)
@@ -711,7 +738,7 @@ def train():
         if global_step % args.i_print == 0 or global_step == args.num_iterations:
             tqdm.write(f"[TRAIN] Iter: {global_step} Loss: {loss.item()}  PSNR: {psnr.item()}")
 
-        tensorboard_writer.add_scalar(f"MSE, train", loss.item(), global_step)
+        tensorboard_writer.add_scalar(f"MSE, train", img_loss, global_step)
         # if args.N_importance > 0:
         #     tf.contrib.summary.scalar('psnr0', psnr0)
         if args.model == 'volsdf':

volsdf.py
@@ -1,6 +1,7 @@
 import numpy as np
 import torch
 from torch import nn
+import radam # pip install git+https://github.com/LiyuanLucasLiu/RAdam/
 
 from run_nerf import DEBUG
 import nerf
@@ -85,26 +86,35 @@ class ImplicitNetwork(nn.Module):
 
         return x
 
-    def predict_sdf(self, x, compute_normal=False):
+    def predict_sdf(self, coord, compute_normal=False):
+        """
+        coord:
+            `torch.Tensor`, shape = `(..., 3)`
+        compute_normal:
+            `bool`
+            If `True`, also return SDF gradient.
+        """
         if compute_normal:
-            x.requires_grad_(True)
+            coord.requires_grad_(True)
             maybe_enable_grad = torch.enable_grad
         else:
             maybe_enable_grad = contextlib.nullcontext
 
         with maybe_enable_grad():
-            prediction = self.forward(x)
+            prediction = self.forward(coord)
             sdf, latent_features = prediction[..., :1], prediction[..., 1:]
 
             if compute_normal:
-                d_output = torch.ones_like(sdf, requires_grad=False, device=sdf.device)
-                sdf_normal = torch.autograd.grad(
-                    outputs=sdf,
-                    inputs=x,
-                    grad_outputs=d_output,
-                    create_graph=True,
-                    retain_graph=True,
-                    only_inputs=True)[0]
+                # Prevent `coord.grad` from getting overwritten/accumulated
+                old_coord_grad = coord.grad # almost always `None`
+
+                d_output = torch.ones_like(sdf)
+                sdf.backward(
+                    d_output, create_graph=self.training, inputs=[coord])
+                sdf_normal = coord.grad
+
+                coord.grad = old_coord_grad
+
                 return sdf, latent_features, sdf_normal
             else:
                 return sdf, latent_features
@@ -187,6 +197,8 @@ class VolSDF(nn.Module):
         self.embedding_viewdirs_fn, embedding_dims_viewdirs = \
             nerf.get_embedder(args.multires_views, args.i_embed)
 
+        self.r = args.r
+
         # TODO move to args
         feature_vector_size = 256
 
@@ -214,6 +226,7 @@ class VolSDF(nn.Module):
 
     def forward(self, coord, view_direction):
         sdf, latent_features, sdf_normal = self.f.predict_sdf(coord, compute_normal=True)
+        sdf = torch.minimum(sdf, self.r - coord.norm(2, dim=-1, keepdim=True))
 
         light_field = self.L(coord, sdf_normal, view_direction, latent_features)
 
@@ -244,7 +257,7 @@ def render_rays(ray_batch, # of length <= `args.chunk`
                 perturb=0.,
                 white_bkgd=False,
                 raw_noise_std=0.,
-                verbose=False):
+                r=3.0):
     """Volumetric rendering.
     Args:
       ray_batch: array of shape [N, ...]. All information necessary
@@ -252,42 +265,33 @@ def render_rays(ray_batch, # of length <= `args.chunk`
         dist, max dist, and unit-magnitude viewing direction.
       network_fn: not used, only needed for saving weights, TODO remove.
       network_query_fn: instance of `VolSDF` wrapped in `nerf.batchify()`
-      N_samples: int. Number of different times to sample along each ray.
+      N_samples: int. Number samples along each ray.
       retraw: bool. If True, include model's raw, unprocessed predictions.
       lindisp: bool. If True, sample linearly in inverse depth rather than in depth.
       perturb: float, 0 or 1. If non-zero, each ray is sampled at stratified
         random points in time.
       white_bkgd: bool. If True, assume a white background.
-      raw_noise_std: ...
-    Returns:
-      rgb_map: [num_rays, 3]. Estimated RGB color of a ray. Comes from fine model.
+      raw_noise_std: random perturbation for density values (as in NeRF).
+    Returns a dict with these keys:
+      rgb_map: [num_rays, 3]. Estimated RGB color of a ray.
       disp_map: [num_rays]. Disparity map. 1 / depth.
-      acc_map: [num_rays]. Accumulated opacity along each ray. Comes from fine model.
-      raw: [num_rays, num_samples, 4]. Raw predictions from model.
-      rgb0: See rgb_map. Output for coarse model.
-      disp0: See disp_map. Output for coarse model.
-      acc0: See acc_map. Output for coarse model.
-      z_std: [num_rays]. Standard deviation of distances along ray for each
-        sample.
+      acc_map: [num_rays]. Accumulated opacity along each ray.
+      raw: [num_rays, num_samples, 4]. Raw predictions from model (R, G, B, density).
     """
     N_rays = ray_batch.shape[0]
     rays_o, rays_d = ray_batch[..., 0:3], ray_batch[..., 3:6] # [N_rays, 3] each
     viewdirs = ray_batch[..., -3:] if ray_batch.shape[-1] > 8 else None # [N_rays, 3]
-    bounds = torch.reshape(ray_batch[..., 6:8], [-1,1,2])
-    near, far = bounds[...,0], bounds[...,1] # [-1,1] # [N_rays, 1] each
+
+    # see "Modeling the background" in Section B.3
+    M = 2 * r
+    near, far = 0, M
 
     # Normalize ray directions. Just to be safe -- not sure if this is really
     # needed (e.g. this wasn't done in 'nerf-pytorch')
     rays_d_norm = rays_d.norm(dim=-1, keepdim=True).clamp(min=1e-4)
-    near *= rays_d_norm
-    far *= rays_d_norm
     rays_d = rays_d / rays_d_norm
 
-    # TODO implement proper sampling
-    # Get sampling Τ (Algorithm 1)
-    # r = 3.0 # TODO move to args
-    # M = 2 * r # TODO move to args
-    # t_vals = torch.linspace(0., M, steps=N_samples)
+    # TODO implement proper sampling Τ (see Algorithm 1)
 
     t_vals = torch.linspace(0., 1., steps=N_samples)
     if not lindisp:
@@ -313,9 +317,13 @@ def render_rays(ray_batch, # of length <= `args.chunk`
     viewdirs = viewdirs[:, None].expand(pts.shape)
 
     # Predict light field and density at points sampled on rays
-    rgb_and_density, sdf_normal = network_query_fn(pts, viewdirs)
+    rgb_and_density, _ = network_query_fn(pts, viewdirs)
+
+    points_for_eikonal_loss = torch.empty((N_rays, 3), device=pts.device).uniform_(-r/2, r/2)
+    _, _, sdf_normal_for_eikonal_loss = network_fn.f.predict_sdf(
+        points_for_eikonal_loss, compute_normal=True)
     # rgb_and_density: [N_rays, N_samples, 4]
-    # sdf_normal: [N_rays, N_samples]
+    # sdf_normal: [N_rays, N_samples, 3]
 
     # Convert predictions to pixel values with numerical integration
     rgb_map, disp_map, acc_map, weights, depth_map = nerf.raw2outputs(
@@ -326,7 +334,7 @@ def render_rays(ray_batch, # of length <= `args.chunk`
     ret = {'rgb_map' : rgb_map, 'disp_map' : disp_map, 'acc_map' : acc_map}
     if retraw:
         ret['raw'] = rgb_and_density
-    ret['sdf_normal'] = sdf_normal
+    ret['sdf_normal'] = sdf_normal_for_eikonal_loss
 
     for k in ret:
         if DEBUG and (torch.isnan(ret[k]).any() or torch.isinf(ret[k]).any()):
@@ -339,8 +347,11 @@ def create_model(args):
     model = VolSDF(args)
 
     # Create optimizer
-    grad_vars = list(model.parameters())
-    optimizer = torch.optim.Adam(params=grad_vars, lr=args.lrate, betas=(0.9, 0.999))
+    grad_vars = model.parameters()
+    if args.optimizer == 'adam':
+        optimizer = torch.optim.Adam(params=grad_vars, lr=args.lrate, betas=(0.9, 0.999))
+    elif args.optimizer == 'radam':
+        optimizer = radam.RAdam(params=grad_vars, lr=args.lrate)
 
     start = 0
 
@@ -387,6 +398,7 @@ def create_model(args):
         'white_bkgd' : args.white_bkgd,
         'raw_noise_std' : args.raw_noise_std,
         'render_rays_fn': render_rays,
+        'r': args.r,
     }
 
     # NDC only good for LLFF-style forward facing data
@@ -398,4 +410,4 @@ def create_model(args):
     render_kwargs_test = {k : render_kwargs_train[k] for k in render_kwargs_train}
     # render_kwargs_test['raw_noise_std'] = 0.
 
-    return render_kwargs_train, render_kwargs_test, start, grad_vars, optimizer
+    return render_kwargs_train, render_kwargs_test, start, optimizer